60d69c667674857529732205090d23559ec70076
myfreeer <myfreeer@users.noreply.github.com>
2023-10-29 11:09:39 +0800

    npm-doc: refactor life-cycle to have multiple prefix

--- src/nodejs-cn-npm/life-cycle.ts
+++ src/nodejs-cn-npm/life-cycle.ts
@@ -3,30 +3,56 @@ import type {
   ProcessingLifeCycle
 } from 'website-scrap-engine/lib/life-cycle/types';
 import {defaultLifeCycle} from 'website-scrap-engine/lib/life-cycle';
-import {
-  defaultDownloadOptions,
-  DownloadOptions
+import type {
+  DownloadOptions,
+  StaticDownloadOptions
 } from 'website-scrap-engine/lib/options';
+import {defaultDownloadOptions,} from 'website-scrap-engine/lib/options';
 import type {Cheerio} from 'website-scrap-engine/lib/types';
 import {preProcessHtml} from './process-html';
+import type {
+  PipelineExecutor
+} from 'website-scrap-engine/lib/life-cycle/pipeline-executor';
+import type {
+  DownloaderWithMeta
+} from 'website-scrap-engine/lib/downloader/types';
 
-const skipExternalLink = (res: Resource, element: Cheerio | null) => {
+const initialUrl: string[] = [];
+
+function init(
+  pipeline: PipelineExecutor, downloader?: DownloaderWithMeta
+) {
+  const prefix = downloader?.options.meta.prefix as string;
+  if (!prefix) {
+    throw new TypeError('init: meta.prefix is required, example val: eslint.nodejs.cn');
+  }
+  const url = `https://${prefix}/`;
+  initialUrl.push(url);
+}
+
+function skipExternalLink(
+  res: Resource, element: Cheerio | null,
+  parent: Resource | null,
+  options: StaticDownloadOptions
+) {
+  const prefix = options.meta.prefix as string;
   const url = res.url;
-  if (!url.startsWith('http://nodejs.cn/npm/') &&
-    !url.startsWith('https://nodejs.cn/npm/') &&
+  if (!url.startsWith(`http://${prefix}/`) &&
+    !url.startsWith(`https://${prefix}/`) &&
     element?.is('a')) {
     return;
   }
   return res;
-};
+}
 
 const lifeCycle: ProcessingLifeCycle = defaultLifeCycle();
+lifeCycle.init.push(init);
 lifeCycle.processBeforeDownload.push(skipExternalLink);
 lifeCycle.processAfterDownload.unshift(preProcessHtml);
 const options: DownloadOptions = defaultDownloadOptions(lifeCycle);
 options.maxDepth = 4;
 options.concurrency = 12;
-options.initialUrl = ['http://nodejs.cn/npm/'];
+options.initialUrl = initialUrl;
 options.req.headers = {
   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
     '(KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
