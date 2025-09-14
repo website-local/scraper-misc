9861a501a3e496afc3228f9f18d456d535ff7dd6
myfreeer <myfreeer@users.noreply.github.com>
2025-09-14 18:40:08 +0800

    infra: esm dirname fix

--- src/nodejs-cn/create-downloader.ts
+++ src/nodejs-cn/create-downloader.ts
@@ -3,18 +3,23 @@ import {promises as fs} from 'fs';
 import {mkdirpSync as mkdir} from 'mkdirp';
 import {SingleThreadDownloader} from 'website-scrap-engine/lib/downloader/single.js';
 import type {StaticDownloadOptions} from 'website-scrap-engine/lib/options.js';
+import {fileURLToPath} from 'node:url';
 
-const defaultApiPath = 'api/v22';
+const defaultApiPath = 'api/v24';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function createDownloader(
   overrideOptions: Partial<StaticDownloadOptions>
 ): Promise<SingleThreadDownloader> {
   const api = overrideOptions?.meta?.nodeApiPath as string || defaultApiPath;
   if (api !== defaultApiPath) {
-    overrideOptions.initialUrl = [`http://nodejs.cn/${api}/`];
+    overrideOptions.initialUrl = [`https://nodejs.cn/${api}/`];
   }
   const downloader: SingleThreadDownloader =
-    new SingleThreadDownloader(path.join(__dirname, 'life-cycle'), overrideOptions);
+    new SingleThreadDownloader(
+      'file://' + path.join(__dirname, 'life-cycle.js'), overrideOptions);
   return downloader.init.then(() => {
     downloader.start();
     // if (overrideOptions?.meta?.replaceNodeApiPath) {
