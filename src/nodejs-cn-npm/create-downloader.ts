6785699a61feed0730f0174b87f2885a45cb0461
myfreeer <myfreeer@users.noreply.github.com>
2025-09-14 10:07:38 +0800

    infra: esm dirname fix

--- src/nodejs-cn-npm/create-downloader.ts
+++ src/nodejs-cn-npm/create-downloader.ts
@@ -1,6 +1,10 @@
 import path from 'path';
 import {SingleThreadDownloader} from 'website-scrap-engine/lib/downloader/single.js';
 import type {StaticDownloadOptions} from 'website-scrap-engine/lib/options.js';
+import {fileURLToPath} from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function createDownloader(
   overrideOptions: Partial<StaticDownloadOptions>
